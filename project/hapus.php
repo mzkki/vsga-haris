00a4cf26484cb86eba47ad23d0747dcb66f5f38c
mzkki <08harismu@gmail.com>
2022-08-16 20:38:01 +0800

    add delete feature project

--- project/hapus.php
+++ project/hapus.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "Halaman ini masih dikembangkan <br>";
-echo 'Klik disini untuk <a href="anggota.php">kembali </a> <br>';
-echo $_GET['idanggota'];
+include 'dbconn.php';
+
+$idAnggota = $_GET['idanggota'];
+
+$data = mysqli_query($conn, "select idanggota from tb_anggota where idanggota='$idAnggota'");
+$d = mysqli_fetch_array($data);
+
+if ($d !== null) {
+  mysqli_query($conn, "delete from tb_anggota where idanggota='$idAnggota'");
+
+  header("location: anggota.php");
+} else {
+  echo "Data tidak ditemukan <br>";
+  echo 'Klik disini untuk <a href="anggota.php">kembali </a><br>';
+}
